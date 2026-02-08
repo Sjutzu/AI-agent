22909cd57a7d286674cd94dac5cc5dcb056232d3
Sjutzu <daniel.zentlik6@gmail.com>
2026-02-08 03:50:34 +0100

    first generated content

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("API key not found")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents="Tell me something about LLM")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
